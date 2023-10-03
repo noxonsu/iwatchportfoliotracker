54e251689eb842a6d41907e734e0bd2d08df284d
Никита Иванов <nivanov129@gmail.com>
2023-10-03 17:50:08 +0400

    Initial Commit

---
+++ Onout Watch App/OnoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnoutApp.swift
+//  Onout Watch App
+//
+//  Created by Никита Иванов on 03.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Onout_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
