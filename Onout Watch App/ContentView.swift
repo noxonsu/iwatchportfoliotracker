c24745bb2c34688381236d73c43fe5b8059e0bea
Noxon <2914674+noxonsu@users.noreply.github.com>
2023-10-18 11:17:37 +0300

    Update ContentView.swift

--- Onout Watch App/ContentView.swift
+++ Onout Watch App/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  Onout Watch App
-//
-//  Created by Никита Иванов on 03.10.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
@@ -12,28 +5,42 @@ struct ContentView: View {
 
     @State private var inputString = ""
     @State private var showingInputDialog = false
-    
+
     var body: some View {
         VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
             Text("Balance:")
                 .font(.headline)
                 .padding()
             Text("$\(Int(viewModel.balance))")
                 .font(.title)
                 .padding()
-            
-            TextField("Enter new wallet", text: $inputString, onCommit: {
-                viewModel.updateWallet(wallet: inputString)
-                
-            })
-            .textFieldStyle(.plain)
-    
+
+            Button("Add new wallet") {
+                showingInputDialog = true
+            }
+            .actionSheet(isPresented: $showingInputDialog) {
+                ActionSheet(title: Text("Choose input method"), buttons: [
+                    .default(Text("Enter on iPhone")) {
+                        // Present a view or a screen on iPhone for input
+                    },
+                    .default(Text("Enter on Watch")) {
+                        // Here you can retain the existing logic or modify as needed for the watch input
+                    },
+                    .cancel()
+                ])
+            }
             .padding()
         }
+        .padding()
     }
 }
 
-
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
